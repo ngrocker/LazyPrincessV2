fd65712d472370c241ce051e3398d73713a06e2c
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2023-01-24 19:32:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://{URL}/shortLink'
+    url = f'http://mallustream.42web.io/shortLink'
     params = {'token': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
@@ -397,8 +397,8 @@ async def get_shortlink(link):
                     return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://{URL}/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'http://mallustream.42web.io/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://{URL}/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'http://mallustream.42web.io/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
